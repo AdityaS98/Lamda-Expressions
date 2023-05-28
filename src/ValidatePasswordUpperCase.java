10a02514c6f045472674f24edef4f02cf0e770eb
AdityaS98 <adityasharmalko1998@gmail.com>
2023-05-28 18:11:25 +0530

    Adding a programme for validating password with uperrcase letter

---
+++ src/ValidatePasswordUpperCase.java
@@ -0,0 +1,22 @@
+package com.expression;
+
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class ValidatePasswordUpperCase {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your password: ");
+        String password = scanner.nextLine();
+
+        Predicate<String> isValidPassword = p -> p.matches("[a-z]{7,}[A-Z]{1}");
+
+        if (isValidPassword.test(password)) {
+            System.out.println("Valid password!");
+        } else {
+            System.out.println("Invalid password!");
+        }
+    }
+
+}
